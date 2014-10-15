7edccd1042373d3c1c3b6a4faa381c54bf1bff33
Oliver <oturnbull1@gmail.com>
2014-10-15 18:42:56 +0100

    commit

--- src/com/literalstudios/sleepystories/MyFragment.java
+++ src/com/literalstudios/sleepystories/MyFragment.java
@@ -1,15 +1,20 @@
 package com.literalstudios.sleepystories;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
-public class MyFragment extends ListFragment{
-
-	public onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState){
-		yadadada
+public class MyFragment extends Fragment{
+	
+	public MyFragment(){}
+	public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		
+		View rootView = inflater.inflate(R.layout.my_fragment, container, false);
+		return rootView;
 	}
 	
 }
