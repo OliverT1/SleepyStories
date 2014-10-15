a4132224ab06c2ce2d53353fdc60e5db73ff9919
Oliver <oturnbull1@gmail.com>
2014-10-15 18:18:05 +0100

    first

---
+++ src/com/literalstudios/sleepystories/MyFragment.java
@@ -0,0 +1,15 @@
+package com.literalstudios.sleepystories;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class MyFragment extends ListFragment{
+
+	public onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState){
+		
+		
+	}
+	
+}
