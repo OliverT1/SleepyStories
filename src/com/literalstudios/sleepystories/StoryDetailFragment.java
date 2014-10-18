69c82ec91de52ca298e5a964060f6d22189a32a1
OliverT1 <oturnbull1@gmail.com>
2014-10-18 10:17:43 +0100

    after detail fragment

---
+++ src/com/literalstudios/sleepystories/StoryDetailFragment.java
@@ -0,0 +1,12 @@
+package com.literalstudios.sleepystories;
+
+public class StoryDetailFragment {
+	
+	Story story;
+	
+	//required no args constructor for fragment
+	public StoryDetailFragment(){}
+	
+	
+	
+}
